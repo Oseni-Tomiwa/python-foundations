c495cfe915cacac66062f0da9ec7dc9729423142
Tomiwa Oseni <omooseni2018@gmail.com>
2026-08-20 22:44:52 +0100

    Dictionaries

---
+++ lesson_8_Dictionaries.py
@@ -0,0 +1,10 @@
+# study_session = {
+#   "topic": "Python",
+#   "hours": 3,
+#   "complected": True
+# }
+
+# print(study_session["topic"])
+# print(study_session["hours"])
+
+# Adding dictionary data
